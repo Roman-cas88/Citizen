8dbc4cc01f85cf728c9461fc847e13066f9519a0
Roman-cas88 <orekhovskiy.roman@gmail.com>
2022-09-07 15:09:00 +0200

    add css, check on wrong input

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 import Citizen from "./citizen.js";
 import Address from "./address.js";
 
+// Drop menu
+document.getElementById("drpMenu").onclick = () => {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+
 // Create <main>
 const newMain = document.createElement('main');
 let foot = document.getElementsByTagName('footer')[0];
@@ -11,38 +29,33 @@ foot.before(newMain);
 const addButtonCreate = document.createElement('button');
 let main = document.getElementById('main');
 addButtonCreate.innerText = "Create";
+addButtonCreate.className = "btnCreate"
 addButtonCreate.id = "buttonCreate";
 addButtonCreate.onclick = function () {
     funcCreate()
 };
 main.append(addButtonCreate);
 
-
-// <button> Create citizen
-const addButtonCreateCitizen = document.createElement('button');
-addButtonCreateCitizen.innerText = "Create citizen";
-addButtonCreateCitizen.id = "buttonCreateCitizen";
-addButtonCreateCitizen.onclick = function () {
-    funcCreateCitizen()
-};
-
-// <button> Register Citizen
-const addButtonRegisterCitizen = document.createElement('button');
-addButtonRegisterCitizen.innerText = "Register citizen";
-addButtonRegisterCitizen.id = "buttonRegisterCitizen";
-addButtonRegisterCitizen.onclick = function () {
-    funcRegister()
-};
-
 // Function to add <button> Create citizen with form and remove <button> Create
 function funcCreate() {
-    foot.before(addButtonCreateCitizen);
+    // <button> Create citizen
+    const addButtonCreateCitizen = document.createElement('button');
+    addButtonCreateCitizen.innerText = "Create citizen";
+    addButtonCreateCitizen.className = "btnCreate"
+    addButtonCreateCitizen.id = "buttonCreateCitizen";
+    addButtonCreateCitizen.onclick = function () {
+        funcCreateCitizen()
+    };
+    main.append(addButtonCreateCitizen);
     document.getElementById('buttonCreate').remove();
     const formCitizen = () => {
         return `
-        <p>Name: <input  id="name"></p>
-        <p>Surname: <input id="sname"></p>
-        <p>Age: <input type="number" id="age"></p>
+        <p>Name:</p>
+        <input class="text" id="name">
+        <p>Surname:</p>
+        <input class="text" id="sname">
+        <p>Age:</p>
+        <input class="number" type="number" id="age">
         `
     }
     let addFormCitizen = document.createElement('div')
@@ -52,77 +65,147 @@ function funcCreate() {
 
 };
 let citizen1 = {};
+let pCheck = document.createElement('p');
+pCheck.id = "pCheck";
+
 
 // function to add address
 function funcCreateCitizen() {
+    main.append(pCheck);
+    if (document.getElementById("name").value == 0 ||
+        isFinite(document.getElementById("name").value) ||
+        document.getElementById("name").value.length >= 20) {
+        document.getElementById('pCheck').innerHTML = "Please check the value";
+        document.getElementById('name').style.borderColor = "red";
+        document.getElementById('sname').style.borderColor = "black";
+        document.getElementById('age').style.borderColor = "black";
+        return;
+    }
+
+    if (document.getElementById("sname").value == 0 ||
+        isFinite(document.getElementById("sname").value) ||
+        document.getElementById("sname").value.length >= 20) {
+        document.getElementById('pCheck').innerHTML = "Please check the value";
+        document.getElementById('sname').style.borderColor = "red";
+        document.getElementById('name').style.borderColor = "black";
+        document.getElementById('age').style.borderColor = "black";
+        return;
+    }
+
+    if (document.getElementById("age").value.length >= 3 ||
+        document.getElementById("age").value == 0) {
+        document.getElementById('pCheck').innerHTML = "Please check the value";
+        document.getElementById('age').style.borderColor = "red";
+        document.getElementById('sname').style.borderColor = "black";
+        document.getElementById('name').style.borderColor = "black";
+        return;
+    }
+    document.getElementById('pCheck').innerHTML = "";
+
     // Create citizen1
     citizen1 = new Citizen(document.querySelector('#name').value, document.querySelector('#sname').value,
         document.querySelector('#age').value, false)
     document.getElementById('divFormCitizen').remove();
     document.getElementById('buttonCreateCitizen').remove();
 
-    const formAddress = () => {
-        return `
-<p>Country: <input  id="country"></p>
-<p>City: <input id="city"></p>
-<p>Street: <input id="street"></p>
-<p>Number of house: <input type="number" id="numHouse"></p>
+    if (citizen1.registered == false) {
+        const formAddress = () => {
+
+            return `
+<p>Country:</p> <input id="country">
+<p>City:</p> <input id="city">
+<p>Street:</p> <input id="street">
+<p>Number of house:</p> <input id="numHouse">
 `
-    }
-    let addFormAddress = document.createElement('div')
-    addFormAddress.id = "divFormAddress";
-    addFormAddress.innerHTML = formAddress();
-    main.append(addFormAddress);
-
-    foot.before(addButtonRegisterCitizen);
-
-};
-
-
-// Function to registered citizen
-const funcRegister = () => {
-    if (citizen1.registered == false)  {
-            // Create citizen1
-            const address1 = new Address(document.querySelector('#country').value, document.querySelector('#city').value,
-                document.querySelector('#street').value, document.querySelector('#numHouse').value)
-            console.log(address1);
-            citizen1.register(address1);
-            alert('Citizen ' + `${citizen1.name} ${citizen1.surname}` + ' registered');
-
-            console.log(`Citizen ${citizen1.name} ${citizen1.surname} ${citizen1.age} registered at: ${citizen1.address.country}, ${citizen1.address.city}, ${citizen1.address.street}, 
-    ${citizen1.address.numHouse}`)
         }
+        // <button> Register Citizen
+        const addButtonRegisterCitizen = document.createElement('button');
+        addButtonRegisterCitizen.innerText = "Register citizen";
+        addButtonRegisterCitizen.className = "btnCreate"
+        addButtonRegisterCitizen.id = "buttonRegisterCitizen";
+        addButtonRegisterCitizen.onclick = function () {
+            funcRegister()
+        };
+
+        let addFormAddress = document.createElement('div')
+        addFormAddress.id = "divFormAddress";
+        addFormAddress.innerHTML = formAddress();
+        main.append(addButtonRegisterCitizen);
+        main.append(addFormAddress);
     };
 
-// const formAddress = () => {
-//     return `
-// <p>Country: <input  id="country"></p>
-// <p>City: <input id="city"></p>
-// <p>Street: <input id="street"></p>
-// <p>Number of house: <input type="number" id="numHouse"></p>
-// `
-// }
-// let addFormAddress = document.createElement('div')
-// addFormAddress.id = "divFormAddress";
-// addFormAddress.innerHTML = formAddress();
-// main.append(addFormAddress)
+    // Function to registered citizen
+    const funcRegister = () => {
+        main.append(pCheck);
+        if (document.getElementById("country").value == 0 ||
+            isFinite(document.getElementById("country").value) ||
+            document.getElementById("country").value.length >= 20) {
+            document.getElementById('pCheck').innerHTML = "Please check the value";
+            document.getElementById('country').style.borderColor = "red";
+            document.getElementById('city').style.borderColor = "black";
+            document.getElementById('street').style.borderColor = "black";
+            document.getElementById('numHouse').style.borderColor = "black";
+            return;
+        }
+
+        if (document.getElementById("city").value == 0 ||
+            isFinite(document.getElementById("city").value) ||
+            document.getElementById("city").value.length >= 20) {
+            document.getElementById('pCheck').innerHTML = "Please check the value";
+            document.getElementById('city').style.borderColor = "red";
+            document.getElementById('country').style.borderColor = "black";
+            document.getElementById('street').style.borderColor = "black";
+            document.getElementById('numHouse').style.borderColor = "black";
+            return;
+        }
+
+        if (document.getElementById("street").value == 0 ||
+            isFinite(document.getElementById("street").value) ||
+            document.getElementById("street").value.length >= 20) {
+            document.getElementById('pCheck').innerHTML = "Please check the value";
+            document.getElementById('street').style.borderColor = "red";
+            document.getElementById('country').style.borderColor = "black";
+            document.getElementById('city').style.borderColor = "black";
+            document.getElementById('numHouse').style.borderColor = "black";
+            return;
+        }
+
+        if (document.getElementById("numHouse").value == 0 ||
+            document.getElementById("numHouse").value.length >= 20) {
+            document.getElementById('pCheck').innerHTML = "Please check the value";
+            document.getElementById('numHouse').style.borderColor = "red";
+            document.getElementById('country').style.borderColor = "black";
+            document.getElementById('city').style.borderColor = "black";
+            document.getElementById('street').style.borderColor = "black";
+            return;
+        }
+        document.getElementById('pCheck').remove();
+
+        // Create citizen1
+        const address1 = new Address(document.querySelector('#country').value, document.querySelector('#city').value,
+            document.querySelector('#street').value, document.querySelector('#numHouse').value)
+        citizen1.register(address1);
 
 
+        console.log(`${citizen1.name} ${citizen1.surname} ${citizen1.age} ${citizen1.address.country}, ${citizen1.address.city} ${citizen1.address.street}, 
+    ${citizen1.address.numHouse}`)
+        document.getElementById('divFormAddress').remove();
+        document.getElementById('buttonRegisterCitizen').remove();
+        let newP = document.createElement('p');
+        newP.id = "mewP";
+        newP.innerHTML = `Citizen ${citizen1.name} ${citizen1.surname} registered to: ${citizen1.address.country}, ${citizen1.address.city} ${citizen1.address.street}, 
+    ${citizen1.address.numHouse}`;
 
-// Drop menu
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-// }
+        let btnNew = document.createElement('button')
+        btnNew.className = "btnCreate";
+        btnNew.innerHTML = "Create New Ctitizen";
+        btnNew.id = "btnNew"
+        btnNew.onclick = () => {
+            location.reload()
+        };
 
-// window.onclick = function (event) {
-//     if (!event.target.matches('.dropbtn')) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         var i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// };
+        main.append(newP);
+        newP.after(btnNew);
+        alert('Citizen ' + `${citizen1.name} ${citizen1.surname}` + ' registered');
+    }
+};
